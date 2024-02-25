89f02673245c8d04e3e342978f817a9d53f328bc
Serafim Sukharev <simasuh@gmail.com>
2024-02-25 18:50:41 +0300

    webpack: init

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        compress: true,
+        port: 9000,
+        hot: true
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            "@": path.resolve(__dirname, 'src')
+        }
+    },
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            }
+        ]
+    }
+};
