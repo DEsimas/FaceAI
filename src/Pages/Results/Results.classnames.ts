077b55077be89cfb143586998639b58a814d024e
Serafim Sukharev <simasuh@gmail.com>
2024-03-01 01:48:07 +0300

    page(results): images preview

--- src/Pages/Results/Results.classnames.ts
+++ src/Pages/Results/Results.classnames.ts
@@ -4,3 +4,4 @@ export const resultsCn = cn('Results');
 export const cnResults = resultsCn();
 export const cnResultsBack = resultsCn('Back');
 export const cnResultsTable = resultsCn('Table');
+export const cnResultsImages = resultsCn('Images');
--- src/Pages/Results/Results.tsx
+++ src/Pages/Results/Results.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../../Components/Button';
-import { cnResults, cnResultsBack, cnResultsTable } from './Results.classnames';
+import { cnResults, cnResultsBack, cnResultsImages, cnResultsTable } from './Results.classnames';
 import { Header } from '../../Components/Header';
+import { Table } from '../../Components/Table';
 import type { ResultProps } from './Results.typings';
 
 import './Results.scss';
-import { Table } from '../../Components/Table';
 
 export function Result(props: ResultProps) {
     const { files, image_ids, table, goBack } = props;
+
+    const [urls, setUrls] = useState<Record<string, string>>({});
+
+    useEffect(() => {
+        const n: Record<string, string> = {}
+        image_ids.forEach(id => n[id] = URL.createObjectURL(files[id]))
+        setUrls(n);
+    }, [setUrls, files]);
 
     return (
         <div className={cnResults}>
@@ -25,7 +33,19 @@ export function Result(props: ResultProps) {
                 table={table}
                 className={cnResultsTable}
             />
-            {/* Slider */}
+            <div className={cnResultsImages}>
+                {
+                    image_ids.map(
+                        (id) => {
+                            const file = files[id];
+                            return <div key={file.name}>
+                                <div>{file.name}</div>
+                                <img alt={file.name} src={urls[id]} />
+                            </div>
+                        }
+                    )
+                }
+            </div>
         </div>
     );
 }
