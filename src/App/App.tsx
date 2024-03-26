e46f6e9064447005d555c2c71ad29ef882b7659b
Serafim Sukharev <simasuh@gmail.com>
2024-03-26 21:23:46 +0300

    load: images for gallery test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -146,27 +146,63 @@ export function App() {
                 items={[
                     {
                         id: '1',
-                        width: 100,
-                        height: 100,
-                        element: <div style={{ width: '100%', height: '100%', backgroundColor: 'red' }} />
+                        width: 2560,
+                        height: 1440,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://www.enjpg.com/img/2020/genshin-impact-4-scaled-e1603714051110.jpg)' }} />
                     },
                     {
                         id: '2',
-                        width: 100,
-                        height: 100,
-                        element: <div style={{ width: '100%', height: '100%', backgroundColor: 'green' }} />
+                        width: 1920,
+                        height: 1080,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://twinfinite.net/wp-content/uploads/2021/01/Genshin-Impact-2-scaled.jpg)' }} />
                     },
                     {
                         id: '3',
-                        width: 100,
-                        height: 100,
-                        element: <div style={{ width: '100%', height: '100%', backgroundColor: 'blue' }} />
+                        width: 3840,
+                        height: 2160,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://www.gamerguides.com/assets/guides/216/keqing_genshin_impact_uhdpaper.com_4K_3.2972.jpg)' }} />
                     },
                     {
                         id: '4',
-                        width: 100,
-                        height: 100,
-                        element: <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }} />
+                        width: 875,
+                        height: 915,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://progameguides.com/wp-content/uploads/2021/03/Genshin-Impact-Character-Ganyu.jpg)' }} />
+                    },
+                    {
+                        id: '5',
+                        width: 1280,
+                        height: 1280,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://www.mobilegamer.com.br/wp-content/uploads/2020/11/genshhin-impact-atualizacao-1.2-3.jpg)' }} />
+                    },
+                    {
+                        id: '6',
+                        width: 900,
+                        height: 1270,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://i.pinimg.com/originals/a3/08/4e/a3084eff8fd3032a2b233071746d19fe.png)' }} />
+                    },
+                    {
+                        id: '7',
+                        width: 1577,
+                        height: 1234,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2020/10/genshin-impact-xingqui_2.png)' }} />
+                    },
+                    {
+                        id: '8',
+                        width: 1280,
+                        height: 720,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://cdn.gamerjournalist.com/primary/2021/01/Genshin-Impact-Ganyu-Talents-Constellations-and-Ascension-Materials.jpg)' }} />
+                    },
+                    {
+                        id: '9',
+                        width: 1920,
+                        height: 1080,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://ftw.usatoday.com/wp-content/uploads/sites/90/2021/10/genshin-impact-traveler.jpg?resize=1024)' }} />
+                    },
+                    {
+                        id: '10',
+                        width: 1400,
+                        height: 700,
+                        element: <div style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundImage: 'url(https://static2.gamerantimages.com/wordpress/wp-content/uploads/2021/01/genshin-impact-mountain.jpg)' }} />
                     },
                 ]}
             />
