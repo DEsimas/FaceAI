8620784382297413d5584fa455506ecb93207d6c
Serafim Sukharev <simasuh@gmail.com>
2024-04-13 14:36:21 +0300

    design: uoload button

--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -14,7 +14,7 @@ export function Counter(props: CounterProps) {
             <div className={cnCounterText}>
                 {window.innerWidth <= 420 ?
                     `${value} из ${max}` :
-                    `выбрано ${value} ${declOfNum(value, ['лицо', 'лица', 'лиц'])} из ${max} возможных`
+                    `Выбрано ${value} ${declOfNum(value, ['лицо', 'лица', 'лиц'])} из ${max} возможных`
                 }
             </div>
         </div>
